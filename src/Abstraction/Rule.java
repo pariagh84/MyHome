5478d7a7a8efb0aab8ae134fa60c15538706ca92
pariagh84 <pariagharakhani@gmail.com>
2025-04-24 23:00:35 +0330

    Update Rule.java

--- src/Abstraction/Rule.java
+++ src/Abstraction/Rule.java
@@ -1,7 +1,7 @@
 package Abstraction;
 
 public record Rule(String deviceName, String time, String action) {
-
+    @Override
     public String toString() {
         return deviceName + " " + time + " " + action;
     }
